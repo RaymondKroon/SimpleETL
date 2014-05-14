e2ea7ef846dd28887f68404e24be857851745cd4
Raymond Kroon <raymond@k3n.nl>
2014-05-14 17:27:20 +0200

    connection string naar input args fatjar naar aparte dir

--- src/main/java/nl/k3n/SimpleETL.java
+++ src/main/java/nl/k3n/SimpleETL.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 import nl.k3n.consumers.CopyToPostgres;
+import nl.k3n.consumers.CountSink;
 import nl.k3n.flatmap.XMLStreamToGeometries;
 import nl.k3n.flatmap.ZipEntriesFromZipEntry;
 import nl.k3n.interfaces.Source;
@@ -17,6 +18,7 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.deegree.feature.types.AppSchema;
@@ -37,19 +39,38 @@ public class SimpleETL {
         
         Options options = new Options();
         options.addOption("h", "help", false, "display help");
+        options.addOption("c", "connectionstring", true, "database connection string");
+        options.addOption("u", "user", true, "database connection username");
+        options.addOption("p", "password", true, "database connection password");
+        options.addOption("T", "test", false, "do a test run without database inserts");
+        Option fileOption = new Option("f", "file", true, "file to process");
+        fileOption.setRequired(true);
+        options.addOption(fileOption);
         
         CommandLineParser parser = new GnuParser();
         
         try {
             CommandLine line = parser.parse(options, args);
-            
-            if (line.hasOption("h") || line.getArgs().length < 1) {
-                HelpFormatter formatter = new HelpFormatter();
-                formatter.printHelp("simpleETL [OPTIONS] [INPUT]", options);
+            HelpFormatter formatter = new HelpFormatter();
+            if (line.hasOption("h")) {
+                formatter.printHelp("simpleETL [OPTIONS]", options);
                 return;
             }
             else {
-                String fileName = line.getArgs()[0];
+                String filename = line.getOptionValue("f").trim();
+                System.out.println(filename);
+                
+                boolean testRun = false;
+                
+                if (!(line.hasOption("c") || line.hasOption("T"))) {
+                    System.err.println("You should provide either a database connectionString or run a test run");
+                    return;
+                }
+                
+                if (line.hasOption("T")) {
+                    testRun = true;
+                }
+                
                 
                 // maps to local xsd so no download
                 GMLAppSchemaReader appSchemaReader = new GMLAppSchemaReader(GMLVersion.GML_31, null, "http://schemas.opengis.net/gml/3.1.1/base/gml.xsd");
@@ -63,12 +84,7 @@ public class SimpleETL {
                     return entry.getEntry().getName().toLowerCase().endsWith(".xml");
                 };
                 
-                PGSimpleDataSource dataSource = new PGSimpleDataSource();
-                dataSource.setUser("postgres");
-                dataSource.setPassword("postgres");
-                dataSource.setUrl("jdbc:postgresql://localhost:5432/gisdb");
-                
-                try (Source<SourcedZipEntry> src = new ZipFileSource(new File(fileName))) {
+                try (Source<SourcedZipEntry> src = new ZipFileSource(new File(filename))) {
                     
                     Stream<byte[]> pipeline = src.stream().unordered()
                             .filter(zipFilter)
@@ -81,13 +97,32 @@ public class SimpleETL {
                     
                     //CountSink sink = new CountSink(100000);
                     
-                    CopyToPostgres copy = new CopyToPostgres((PGConnection)unchecked(() -> dataSource.getConnection()), 
-                            "bag_copy_test.geometry", 100000, 4, 100000);
+                    if (!testRun) {
                     
-                    pipeline.forEach(copy);
-                    
-                    // flush remainder
-                    copy.waitForFlush();
+                        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+                        
+                        //dataSource.setUrl("jdbc:postgresql://localhost:5432/gisdb");
+                        dataSource.setUrl(line.getOptionValue("c"));
+                        if (line.hasOption("u")) {
+                            dataSource.setUser(line.getOptionValue("u"));
+                        }
+                        
+                        if (line.hasOption("p")) {
+                            dataSource.setPassword(line.getOptionValue("p"));
+                        }
+                        
+                        CopyToPostgres copy = new CopyToPostgres((PGConnection)unchecked(() -> dataSource.getConnection()), 
+                                "bag_copy_test.geometry", 100000, 4, 100000);
+
+                        pipeline.forEach(copy);
+
+                        // flush remainder
+                        copy.waitForFlush();
+                    }
+                    else {
+                        CountSink count = new CountSink(100000);
+                        pipeline.forEach(count);
+                    }
                     
                 }
                 catch (FileNotFoundException ex) {
@@ -101,6 +136,8 @@ public class SimpleETL {
             
         } catch (ParseException ex) {
             System.err.println("Parsing failed. Reason: " + ex.getMessage());
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("simpleETL [OPTIONS]", options);
         }
         
     }
